a907e20e3f7404081f2cf2c84ec6e1dabbde013f
Maciej Pie <maciejpiech18@gmail.com>
2021-04-22 09:07:59 -0500

    added a database

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require('./app');
+const { syncAndSeed } = require('./db');
 
 const port = process.env.PORT || 1813;
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+const init = async () => {
+  app.listen(port, () => console.log(`listening on port ${port}`));
+  await syncAndSeed();
+};
+
+init();
